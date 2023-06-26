7a49cb74ec0378dfb8ba8ab863f423634541a271
choripunkkk <bas.nahuel@duocuc.cl>
2023-06-25 22:19:58 -0400

    ultimatum

---
+++ core/serializers.py
@@ -0,0 +1,26 @@
+from .models import *
+from rest_framework import serializers
+
+
+class TipoProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= TipoProducto
+        fields= '__all__'
+
+class ProductoSerializer(serializers.ModelSerializer):
+    
+    tipo = TipoProductoSerializer(read_only=True)
+    #aqui añadir claves foraneas
+    class Meta:
+        model= Producto
+        fields= '__all__'
+
+class CuponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Cupon
+        fields= '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields= '__all__'        
